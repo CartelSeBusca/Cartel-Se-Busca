46a0022ea2eed62ac4db2435f28ee158dc79271a
CartelSeBusca <cartelsebuscamx@gmail.com>
2024-09-09 11:02:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ document.getElementById('upload').addEventListener('change', function(event) {
         reader.readAsDataURL(file);
     }
 });
+
+document.getElementById('download').addEventListener('click', function() {
+    const poster = document.querySelector('.poster');
+    html2canvas(poster).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'cartel-se-busca.png';
+        link.click();
+    });
+});
